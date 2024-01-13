7fd4a986cf4d7bb03dfa5fdeb61025396a8c7671
xcelxlorx <xcelxlorx@gmail.com>
2024-01-14 00:43:48 +0900

    feat: Add api error response

--- src/main/java/com/cherryhouse/server/_core/util/ApiResponse.java
+++ src/main/java/com/cherryhouse/server/_core/util/ApiResponse.java
@@ -1,6 +1,8 @@
 package com.cherryhouse.server._core.util;
 
+import com.cherryhouse.server._core.exception.ExceptionCode;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 public class ApiResponse {
 
@@ -10,6 +12,14 @@ public class ApiResponse {
 
     public static <T>ResponseDto<T> success(T response){
         return new ResponseDto<>(200, response, null);
+    }
+
+    public static <T>ResponseDto<T> error(ExceptionCode error){
+        return new ResponseDto<>(error.getStatus().value(), null, error.getMessage());
+    }
+
+    public static <T>ResponseDto<T> error(HttpStatus status, String message){
+        return new ResponseDto<>(status.value(), null, message);
     }
 
     @AllArgsConstructor
