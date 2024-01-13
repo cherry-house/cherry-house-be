6bcfea2de85eac13e2139a21b2af3561f307c7ec
xcelxlorx <xcelxlorx@gmail.com>
2024-01-14 00:44:04 +0900

    feat: Add exception handler

---
+++ src/main/java/com/cherryhouse/server/_core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.cherryhouse.server._core.exception;
+
+import com.cherryhouse.server._core.util.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<?> error(ApiException ex){
+        return ResponseEntity.status(ex.getError().getStatus()).body(ApiResponse.error(ex.getError()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> serverError(Exception ex){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(status).body(ApiResponse.error(status, ex.getMessage()));
+    }
+}
