bf97e4adfe35d832c07fb215d02241afc3177097
xcelxlorx <xcelxlorx@gmail.com>
2024-01-15 13:52:03 +0900

    feat: Add constructor to api exception

--- src/main/java/com/cherryhouse/server/_core/exception/ApiException.java
+++ src/main/java/com/cherryhouse/server/_core/exception/ApiException.java
@@ -1,11 +1,20 @@
 package com.cherryhouse.server._core.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class ApiException extends RuntimeException{
 
     private final ExceptionCode error;
+    private final String message;
+
+    public ApiException(ExceptionCode error, String message){
+        this.error = error;
+        this.message = message;
+    }
+
+    public ApiException(ExceptionCode error){
+        this.error = error;
+        this.message = error.getMessage();
+    }
 }
