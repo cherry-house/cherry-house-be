5f95b7a4dd1654387abafb9cd4835375542797dd
xcelxlorx <xcelxlorx@gmail.com>
2024-01-15 20:32:15 +0900

    fix: Modify page data to record

--- src/main/java/com/cherryhouse/server/_core/util/PageData.java
+++ src/main/java/com/cherryhouse/server/_core/util/PageData.java
@@ -1,15 +1,11 @@
 package com.cherryhouse.server._core.util;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+public record PageData (
+        Long totalDataCnt, //총 데이터 수
+        int totalPages, //총 페이지 수
+        Boolean isLastPage, //마지막 페이지인지
+        Boolean isFirstPage, //첫 페이지인지
+        int requestPage, //요청 페이지 번호
+        int requestSize //요청 페이지 번호에 있는 데이터 수
+){}
 
-@AllArgsConstructor
-@Getter
-public class PageData {
-    private Long totalDataCnt; //총 데이터 수
-    private int totalPages; //총 페이지 수
-    private Boolean isLastPage; //마지막 페이지인지
-    private Boolean isFirstPage; //첫 페이지인지
-    private int requestPage; //요청 페이지 번호
-    private int requestSize; //요청 페이지 번호에 있는 데이터 수
-}
