877d4e4d5456e456b04a6f1c982c2b79bcc63ef4
pigglehyun <sarah0661@pusan.ac.kr>
2024-01-17 13:56:11 +0900

    feat: TokenDTO

---
+++ src/main/java/com/cherryhouse/server/_core/security/dto/TokenDto.java
@@ -0,0 +1,28 @@
+package com.cherryhouse.server._core.security.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class TokenDto {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private String grantType;
+        private String accessToken;
+        private String refreshToken;
+        private Long accessTokenExpiresIn;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Request {
+        private String accessToken;
+        private String refreshToken;
+    }
+}
