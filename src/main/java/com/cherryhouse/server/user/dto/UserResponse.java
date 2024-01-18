1a8c2f0807862e3ab1d65152e49e2c4152181c08
pigglehyun <sarah0661@pusan.ac.kr>
2024-01-18 14:27:23 +0900

    feat: User Response Dto

---
+++ src/main/java/com/cherryhouse/server/user/dto/UserResponse.java
@@ -0,0 +1,28 @@
+package com.cherryhouse.server.user.dto;
+
+import com.cherryhouse.server.user.User;
+import lombok.*;
+
+public class UserResponse {
+
+    //TODO : record 로 변경하기
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class UserResponseDto {
+        private String email;
+        private String username;
+        private String profileImageUrl;
+
+        public static UserResponse.UserResponseDto toDto(User user){
+            return new UserResponse.UserResponseDto(
+                    user.getEmail(),
+                    user.getName(),
+                    user.getProfileImage()
+
+            );
+        }
+
+    }
+}
