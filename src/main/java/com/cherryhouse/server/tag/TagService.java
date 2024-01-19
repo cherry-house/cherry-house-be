b615e5df5b67a562bd8cd31aabc70c24872db9b1
xcelxlorx <xcelxlorx@gmail.com>
2024-01-19 20:51:53 +0900

    refactor: Remove duplicate codes

--- src/main/java/com/cherryhouse/server/tag/TagService.java
+++ src/main/java/com/cherryhouse/server/tag/TagService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,24 +19,24 @@ public class TagService {
 
     @Transactional
     public void create(Post post, List<String> tags){
-        for(String tagName : tags){
-            Optional<Tag> existingTag = tagRepository.findByName(tagName);
+        tags.forEach(tagName -> save(post, tagName));
+    }
 
-            //태그 이름이 있으면 위에서 가져온 태그 그대로 넣고, 없으면 새로운 태그 생성
-            Tag tag = existingTag.orElseGet(() -> {
-                Tag newTag = Tag.builder()
-                        .name(tagName)
-                        .build();
-                tagRepository.save(newTag);
-                return newTag;
-            });
+    private void save(Post post, String tagName) {
+        Tag tag = tagRepository.findByName(tagName)
+                .orElseGet(() -> {
+                    Tag newTag = Tag.builder()
+                            .name(tagName)
+                            .build();
+                    tagRepository.save(newTag);
+                    return newTag;
+                });
 
-            PostTag postTag = PostTag.builder()
-                    .post(post)
-                    .tag(tag)
-                    .build();
-            postTagRepository.save(postTag);
-        }
+        PostTag postTag = PostTag.builder()
+                .post(post)
+                .tag(tag)
+                .build();
+        postTagRepository.save(postTag);
     }
 
     public List<String> getTags(Long postId){
@@ -51,26 +50,10 @@ public class TagService {
     public void update(Post post, List<String> tags){
         List<PostTag> postTags = postTagRepository.findByPostId(post.getId());
 
+        //tags(새로 들어온 태그) 중 가지고 있지 않은 경우만 확인하면 됨
         tags.stream()
-                //tags(새로 들어온 태그) 중 가지고 있지 않은 경우만 확인하면 됨
                 .filter(tagName -> postTags.stream().noneMatch(postTag -> postTag.getTag().getName().equals(tagName)))
-                .forEach(tagName -> {
-                    //태그 테이블에 있을 경우 get 으로 가져오고, 없을 경우 새로운 태그를 생성한 후 가져옴
-                    Tag tag = tagRepository.findByName(tagName)
-                            .orElseGet(() -> {
-                                Tag newTag = Tag.builder()
-                                        .name(tagName)
-                                        .build();
-                                tagRepository.save(newTag);
-                                return newTag;
-                            });
-
-                    PostTag newPostTag = PostTag.builder()
-                            .post(post)
-                            .tag(tag)
-                            .build();
-                    postTagRepository.save(newPostTag);
-                });
+                .forEach(tagName -> save(post, tagName));
 
         //tags(새로 들어온 태그)에 없고 post tag 에 있는 태그 삭제
         postTagRepository.findByPostId(post.getId())
