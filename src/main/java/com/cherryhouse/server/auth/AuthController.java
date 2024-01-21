1a4f6be598e8edf11541da49c988fd37a2076c77
xcelxlorx <xcelxlorx@gmail.com>
2024-01-21 18:26:02 +0900

    feat: Add email to auth controller

--- src/main/java/com/cherryhouse/server/auth/AuthController.java
+++ src/main/java/com/cherryhouse/server/auth/AuthController.java
@@ -1,8 +1,10 @@
 package com.cherryhouse.server.auth;
 
 import com.cherryhouse.server._core.util.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +17,27 @@ public class AuthController {
 
     private final AuthService authService;
 
-    //TODO : validation + errors
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody AuthDto.JoinDto joinDto) {
-        authService.join(joinDto);
+    public ResponseEntity<?> join(@RequestBody @Valid AuthRequest.JoinDto request, Errors errors) {
+        authService.join(request);
         return ResponseEntity.ok().body(ApiResponse.success());
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthDto.LoginDto loginDto) {
-        authService.login(loginDto);
-        return ResponseEntity.ok().body(ApiResponse.success(authService.login(loginDto)));
+    public ResponseEntity<?> login(@RequestBody @Valid AuthRequest.LoginDto request, Errors errors) {
+        authService.login(request);
+        return ResponseEntity.ok().body(ApiResponse.success(authService.login(request)));
+    }
+
+    @PostMapping("/email-verification-code")
+    public ResponseEntity<?> sendVerificationCode(@RequestBody @Valid AuthRequest.SendVerificationCodeDto request, Errors errors) {
+        authService.sendVerificationCode(request);
+        return ResponseEntity.ok().body(ApiResponse.success());
+    }
+
+    @PostMapping("/email-verification")
+    public ResponseEntity<?> emailVerification(@RequestBody @Valid AuthRequest.EmailVerificationDto request, Errors errors) {
+        authService.emailVerification(request);
+        return ResponseEntity.ok().body(ApiResponse.success());
     }
 }
