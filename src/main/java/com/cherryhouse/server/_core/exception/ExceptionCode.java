ba97f902327e30c4aa9a5d11e9e46abb96c6c22b
pigglehyun <sarah0661@pusan.ac.kr>
2024-01-22 23:32:10 +0900

    add: error code

--- src/main/java/com/cherryhouse/server/_core/exception/ExceptionCode.java
+++ src/main/java/com/cherryhouse/server/_core/exception/ExceptionCode.java
@@ -17,7 +17,9 @@ public enum ExceptionCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시글을 찾을 수 없습니다."),
 
     INVALID_REQUEST_DATA(HttpStatus.BAD_REQUEST, "입력이 잘못되었습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 오류가 발생했습니다."),
+
+    FAIL_TO_LOGOUT(HttpStatus.BAD_REQUEST,"로그아웃에 실패함");
 
     private final HttpStatus status;
     private final String message;
