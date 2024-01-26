a08e2efe8dd9470fde90302ee153440432c26454
pigglehyun <sarah0661@pusan.ac.kr>
2024-01-26 16:36:15 +0900

    feat: add user style controller

--- src/main/java/com/cherryhouse/server/user/UserController.java
+++ src/main/java/com/cherryhouse/server/user/UserController.java
@@ -1,14 +1,37 @@
 package com.cherryhouse.server.user;
 
 
+import com.cherryhouse.server._core.security.UserPrincipal;
+import com.cherryhouse.server._core.util.ApiResponse;
+import com.cherryhouse.server.s3.S3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+    @PostMapping("/style")
+    public ResponseEntity<?> uploadStyle(
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+            , @RequestParam("file") List<MultipartFile> file){
+        userService.uploadStyle(file,userPrincipal.getEmail());
+        return ResponseEntity.ok().body(ApiResponse.success());
+    }
+
+    @DeleteMapping("/style")
+    public ResponseEntity<?> deleteStyle(
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+            , @RequestParam("file") String filePath){
+        userService.deleteStyle(filePath,userPrincipal.getEmail());
+        return ResponseEntity.ok().body(ApiResponse.success());
+    }
 
 }
