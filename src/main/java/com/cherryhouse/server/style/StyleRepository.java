cf02059eccf9c6ca33cfb6a011ab8364d86d5b53
pigglehyun <sarah0661@pusan.ac.kr>
2024-01-26 16:37:46 +0900

    feat: add style repository

---
+++ src/main/java/com/cherryhouse/server/style/StyleRepository.java
@@ -0,0 +1,16 @@
+package com.cherryhouse.server.style;
+
+import com.cherryhouse.server.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface StyleRepository extends JpaRepository<Style, Long> {
+
+    @Query("select s.id from Style s " +
+            "where s.user.email = :email " +
+            "and s.imgUrl = :filePath")
+    Optional<Long> findByUserEmailAndFileName(@Param("email") String email, @Param("filePath") String filePath);
+}
