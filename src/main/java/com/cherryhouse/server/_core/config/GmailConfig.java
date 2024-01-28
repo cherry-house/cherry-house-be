db4b1b7a0410429b79bdad4425dea4358946ed28
xcelxlorx <xcelxlorx@gmail.com>
2024-01-28 12:16:56 +0900

    fix: Modify mail config name

--- src/main/java/com/cherryhouse/server/_core/config/GmailConfig.java
+++ src/main/java/com/cherryhouse/server/_core/config/GmailConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-public class MailConfig {
+public class GmailConfig {
 
     @Value("${spring.mail.host}")
     private String host;
