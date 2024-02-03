a93b716a9db15b94117548b8746303772bdac9b0
pigglehyun <sarah0661@pusan.ac.kr>
2024-02-03 11:40:50 +0900

    feat: add config

--- src/main/java/com/cherryhouse/server/_core/config/SecurityConfig.java
+++ src/main/java/com/cherryhouse/server/_core/config/SecurityConfig.java
@@ -54,6 +54,8 @@ public class SecurityConfig {
                authorizeRequests -> authorizeRequests
                        .requestMatchers("/auth/**").permitAll()
                        .requestMatchers("/posts/**").permitAll()
+                       .requestMatchers("/hearts/**").permitAll()
+                       .requestMatchers("/users/**").permitAll()
                        .anyRequest().permitAll()
        );
 
