b557e9267cac88cc1aed6ba14d16bd0f21740f30
pigglehyun <sarah0661@pusan.ac.kr>
2024-02-03 11:42:41 +0900

    feat: add Heart toggle logic

---
+++ src/main/java/com/cherryhouse/server/heart/HeartController.java
@@ -0,0 +1,32 @@
+package com.cherryhouse.server.heart;
+
+
+import com.cherryhouse.server._core.security.UserPrincipal;
+import com.cherryhouse.server._core.util.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/hearts")
+@RequiredArgsConstructor
+public class HeartController {
+
+    private final HeartService heartService;
+
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<?> toggleHeart(@AuthenticationPrincipal UserPrincipal user,
+                                         @PathVariable("postId")Long postId){
+        boolean HeartAdded = heartService.toggleHeart(user.getEmail(), postId);
+
+        String msg = HeartAdded ? "좋아요 추가" : "좋아요 취소";
+        return ResponseEntity.ok().body(ApiResponse.success(msg));
+    }
+
+}
