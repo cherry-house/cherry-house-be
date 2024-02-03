408a951afe9a6d910927c36dd2a57571ca4c2f25
xcelxlorx <xcelxlorx@gmail.com>
2024-02-03 17:08:51 +0900

    fix: Modify path variable to request param

--- src/main/java/com/cherryhouse/server/heart/HeartController.java
+++ src/main/java/com/cherryhouse/server/heart/HeartController.java
@@ -1,17 +1,14 @@
 package com.cherryhouse.server.heart;
 
-
 import com.cherryhouse.server._core.security.UserPrincipal;
 import com.cherryhouse.server._core.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RestController
 @RequestMapping("/hearts")
 @RequiredArgsConstructor
@@ -19,13 +16,11 @@ public class HeartController {
 
     private final HeartService heartService;
 
-
-    @PostMapping("/{postId}")
+    @PostMapping
     public ResponseEntity<?> toggleHeart(@AuthenticationPrincipal UserPrincipal user,
-                                         @PathVariable("postId")Long postId){
-        boolean HeartAdded = heartService.toggleHeart(user.getEmail(), postId);
-
-        String msg = HeartAdded ? "좋아요 추가" : "좋아요 취소";
+                                         @RequestParam("postId") Long postId){
+        boolean heartAdded = heartService.toggleHeart(user.getEmail(), postId);
+        String msg = heartAdded ? "좋아요 추가" : "좋아요 취소";
         return ResponseEntity.ok().body(ApiResponse.success(msg));
     }
 
@@ -35,6 +30,4 @@ public class HeartController {
         HeartResponse.HeartDto response = heartService.getHearts(userPrincipal.getEmail(), pageable);
         return ResponseEntity.ok().body(ApiResponse.success(response));
     }
-
-
 }
