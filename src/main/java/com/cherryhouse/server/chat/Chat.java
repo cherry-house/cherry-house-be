24a35afa1facfb8d3268039bdf83570e3b8e282e
xcelxlorx <xcelxlorx@gmail.com>
2024-02-04 15:09:02 +0900

    feat: Add chat entity

---
+++ src/main/java/com/cherryhouse/server/chat/Chat.java
@@ -0,0 +1,45 @@
+package com.cherryhouse.server.chat;
+
+import com.cherryhouse.server.chatroom.ChatRoom;
+import com.cherryhouse.server.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "chat_tb")
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user; //보낸 유저
+
+    @ColumnDefault("false")
+    private Boolean isRead;
+
+    private String type;
+    private String createdDate;
+
+    @Builder
+    public Chat(String content, ChatRoom chatRoom, User user, String type) {
+        this.content = content;
+        this.chatRoom = chatRoom;
+        this.user = user;
+        this.type = type;
+        this.createdDate = String.valueOf(LocalDateTime.now());
+    }
+}
