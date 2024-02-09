da46abb46d38c2c2aec2cfe057bc6ad69813ef3d
pigglehyun <sarah0661@pusan.ac.kr>
2024-02-09 22:15:48 +0900

    feat: user information

--- src/main/java/com/cherryhouse/server/user/dto/UserRequest.java
+++ src/main/java/com/cherryhouse/server/user/dto/UserRequest.java
@@ -1,6 +1,32 @@
 package com.cherryhouse.server.user.dto;
 
+import jakarta.validation.constraints.*;
+
 public class UserRequest {
+
+    public record updateInfoDto(
+
+            @Email
+            String email,
+            @NotBlank(message = "닉네임은 필수 입력입니다.")
+            @Size(min = 2, max = 15, message = "닉네임은 2글자 이상 15글자 이하로 입력하세요.")
+            @NotNull(message = "닉네임은 필수 입력입니다.")
+            String username,
+            String introduction
+    ){}
+
+    public record updateImgDto(
+            String email,
+            String img
+    ){}
+
+    public record updatePwdDto(
+            String email,
+            @NotBlank(message = "비밀번호는 필수 입력입니다.")
+            @NotNull(message = "비밀번호는 필수 입력입니다.")
+            @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "비밀번호는 영문자와 숫자를 포함하여 8글자 이상이여야 합니다.")
+            String password
+    ){}
 
 
 }
--- src/main/java/com/cherryhouse/server/user/dto/UserResponse.java
+++ src/main/java/com/cherryhouse/server/user/dto/UserResponse.java
@@ -1,27 +1,96 @@
 package com.cherryhouse.server.user.dto;
 
+import com.cherryhouse.server._core.util.PageData;
+import com.cherryhouse.server.heart.HeartResponse;
+import com.cherryhouse.server.post.Post;
+import com.cherryhouse.server.posttag.PostTagMapping;
+import com.cherryhouse.server.style.Style;
 import com.cherryhouse.server.user.User;
 import lombok.*;
 
+import java.util.List;
+
+import static com.cherryhouse.server.posttag.PostTagMapping.getTagsByPostId;
+
 public class UserResponse {
 
-    //TODO : record 로 변경하기
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class UserResponseDto {
-        private String email;
-        private String username;
-        private String profileImageUrl;
-
-        public static UserResponse.UserResponseDto toDto(User user){
-            return new UserResponse.UserResponseDto(
+    public record UsersDto(
+            String email,
+            String username,
+            String profileImg
+    ){
+        public UsersDto(User user){
+            this(
                     user.getEmail(),
                     user.getName(),
-                    user.getProfileImage()
+                    user.getProfileImage());
+        }
+    }
+
+    public record UserDto(
+
+            PageData pageData,
+            String username,
+            String profileImg,
+            String introduction,
+            List<StyleDto> styleList,
+            List<PostDto> postList
+
+    ){
+        public static UserDto of(PageData pageData,
+                                 User user,
+                                 List<Style> styleList,
+                                 List<Post> postList,
+                                 List<PostTagMapping> postTagMappingList
+        ){
+            return new UserDto(
+                    pageData,
+                    user.getName(),
+                    user.getProfileImage(),
+                    user.getIntroduction(),
+                    styleList.stream()
+                            .map(StyleDto::new)
+                            .toList(),
+                    postList.stream()
+                            .map(post -> new PostDto(post, getTagsByPostId(postTagMappingList, post.getId())))
+                            .toList()
 
             );
         }
+
+
+        public record StyleDto(
+                Long id,
+                String img
+        ){
+            public StyleDto(Style style){
+                this(
+                        style.getId(),
+                        style.getImgUrl()
+                );
+            }
+        }
+        public record PostDto (
+                Long id,
+                String title,
+                String location,
+                String address,
+                Integer distance,
+                List<String> tags,
+                List<String> photos
+        ){
+            public PostDto(Post post, List<String> tags) {
+                this(
+                        post.getId(),
+                        post.getTitle(),
+                        null,
+                        null,
+                        null,
+                        tags,
+                        null
+                );
+            }
+        }
     }
+
 }
