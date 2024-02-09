c6e918114e2e18e8a23f7e5ad953471db851b23c
pigglehyun <sarah0661@pusan.ac.kr>
2024-02-09 22:15:30 +0900

    fix: user

--- src/main/java/com/cherryhouse/server/user/User.java
+++ src/main/java/com/cherryhouse/server/user/User.java
@@ -1,5 +1,6 @@
 package com.cherryhouse.server.user;
 
+import com.cherryhouse.server.post.Post;
 import com.cherryhouse.server.style.Style;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -30,19 +31,27 @@ public class User {
     @Column
     private String profileImage;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-    private List<Style> styles;
+    @Column
+    private String introduction;
+
+
+
 
     @Builder
-    public User(Long id, String email, String password, String username, String profileImage,List<Style> styles){
+    public User(Long id, String email, String password, String username, String profileImage, String introduction){
         this.id = id;
         this.email = email;
         this.password = password;
         this.name = username;
         this.profileImage = profileImage;
-        this.styles = styles;
+        this.introduction = introduction;
     }
 
+    public void updateInfo(String username, String introduction){
+        this.name = username;
+        this.introduction = introduction;
+    }
+    public void updateImg(String img) { this.profileImage = img; }
     public void updatePassword(String password){
         this.password = password;
     }
