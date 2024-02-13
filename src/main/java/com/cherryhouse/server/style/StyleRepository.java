d4ffae193f3b29bc594cac0d11024434a96b14ec
xcelxlorx <xcelxlorx@gmail.com>
2024-02-13 19:00:07 +0900

    fix: Modify img url in style repository

--- src/main/java/com/cherryhouse/server/style/StyleRepository.java
+++ src/main/java/com/cherryhouse/server/style/StyleRepository.java
@@ -11,12 +11,12 @@ public interface StyleRepository extends JpaRepository<Style, Long> {
 
     @Query("select s.id from Style s " +
             "where s.user.email = :email " +
-            "and s.imgUrl = :filePath")
+            "and s.accessImgUrl = :filePath")
     Optional<Long> findByUserEmailAndFileName(@Param("email") String email, @Param("filePath") String filePath);
 
     @Query("select s from Style s " +
-            "where s.user.id = :id " +
+            "where s.user.id = :userId " +
             "order by s.id " +
             "desc")
-    List<Style> findByUserId(@Param("id") Long id);
+    List<Style> findByUserId(@Param("userId") Long userId);
 }
