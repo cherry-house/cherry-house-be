a7d23dbd2a2f33554fe364e1e3b1259a48d7d8de
pigglehyun <sarah0661@pusan.ac.kr>
2024-02-29 17:02:45 +0900

    feat: add reserve controller

---
+++ src/main/java/com/cherryhouse/server/reserve/ReserveController.java
@@ -0,0 +1,35 @@
+package com.cherryhouse.server.reserve;
+
+import com.cherryhouse.server._core.security.UserPrincipal;
+import com.cherryhouse.server._core.util.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reserve")
+@RequiredArgsConstructor
+public class ReserveController {
+
+    private final ReserveService reserveService;
+
+    @PostMapping
+    public ResponseEntity<?> reserve(@AuthenticationPrincipal UserPrincipal user,
+                                     @Valid @RequestBody ReserveRequest.makeReserveDto makeReserveDto){
+        reserveService.reserve(user,makeReserveDto);
+        return ResponseEntity.ok().body(ApiResponse.success());
+    }
+
+    @PutMapping("/change")
+    public ResponseEntity<?> updateReserve(@AuthenticationPrincipal UserPrincipal user,
+                                     @Valid @RequestBody ReserveRequest.changeReserveDto changeReserveDto){
+        reserveService.update(user,changeReserveDto);
+        return ResponseEntity.ok().body(ApiResponse.success());
+    }
+
+
+
+
+}
