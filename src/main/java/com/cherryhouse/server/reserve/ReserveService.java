57356c3c976dc259a2d89c48127b8a99bf98531a
pigglehyun <sarah0661@pusan.ac.kr>
2024-03-02 23:26:30 +0900

    feat: get reservation service

--- src/main/java/com/cherryhouse/server/reserve/ReserveService.java
+++ src/main/java/com/cherryhouse/server/reserve/ReserveService.java
@@ -3,20 +3,29 @@ package com.cherryhouse.server.reserve;
 import com.cherryhouse.server._core.exception.ApiException;
 import com.cherryhouse.server._core.exception.ExceptionCode;
 import com.cherryhouse.server._core.security.UserPrincipal;
+import com.cherryhouse.server._core.util.PageData;
 import com.cherryhouse.server.post.Post;
 import com.cherryhouse.server.post.PostRepository;
 import com.cherryhouse.server.post.PostService;
+import com.cherryhouse.server.post.image.ImageMapping;
+import com.cherryhouse.server.post.image.ImageService;
+import com.cherryhouse.server.post.posttag.PostTagMapping;
+import com.cherryhouse.server.post.tag.TagService;
 import com.cherryhouse.server.user.User;
 import com.cherryhouse.server.user.UserRepository;
 import com.cherryhouse.server.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -29,6 +38,8 @@ public class ReserveService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final ReserveRepository reserveRepository;
+    private final TagService tagService;
+    private final ImageService imageService;
 
     @Transactional
     public void reserve(UserPrincipal user, ReserveRequest.makeReserveDto makeReserveDto) {
@@ -46,7 +57,7 @@ public class ReserveService {
         LocalDate createDate = LocalDate.from(LocalDateTime.now());
 
         Reserve reserve = Reserve.builder()
-                .postId(post.getId())
+                .post(post)
                 .provider(provider)
                 .receiver(receiver)
                 .reservationDate(reservationDate)
@@ -75,6 +86,22 @@ public class ReserveService {
                 .orElseThrow( () -> new ApiException(ExceptionCode.RESERVE_NOT_FOUND));
 
         reserve.update(changeReserveDto.reservationDate(),changeReserveDto.status());
+
+    }
+
+    public ReserveResponse.ReserveDto get(String email, Pageable pageable) {
+        Page<Reserve> reserveList = reserveRepository.findAllByUser(email, pageable);
+        List<Post> postList = reserveList.stream()
+                .map(Reserve::getPost)
+                .collect(Collectors.toList());
+
+        PageData pageData = PageData.getPageData(reserveList);
+
+
+        List<PostTagMapping.TagsDto> tagsDtoList = tagService.getTagsDtoList(postList);
+        List<ImageMapping.UrlDto> urlDtoList = imageService.getUrlDtoList(postList);
+
+        return ReserveResponse.ReserveDto.of(pageData,reserveList.getContent(),tagsDtoList,urlDtoList);
 
     }
 }
