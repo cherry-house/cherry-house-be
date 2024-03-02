7f2d48f9794f509aa108adaf6043e520c9c76b8f
pigglehyun <sarah0661@pusan.ac.kr>
2024-03-02 23:15:13 +0900

    feat: get query

--- src/main/java/com/cherryhouse/server/reserve/ReserveRepository.java
+++ src/main/java/com/cherryhouse/server/reserve/ReserveRepository.java
@@ -1,7 +1,10 @@
 package com.cherryhouse.server.reserve;
 
 import com.cherryhouse.server.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,4 +14,11 @@ public interface ReserveRepository extends JpaRepository<Reserve, Long> {
     Optional<Reserve> findByPostIdAndReceiver(Long id, User receiver);
 
     Optional<Reserve> findByPostIdAndProviderAndReceiver(Long id, User provider, User receiver);
+
+    @Query("select r from Reserve r " +
+            "where r.provider.email = :email " +
+            "or r.receiver.email = :email " +
+            "order by r.id " +
+            "desc ")
+    Page<Reserve> findAllByUser(String email, Pageable pageable);
 }
