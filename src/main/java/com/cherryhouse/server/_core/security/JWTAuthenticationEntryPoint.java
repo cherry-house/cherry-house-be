948ccdb14a436a004b89df4606da8a8e9bd42a10
pigglehyun <sarah0661@pusan.ac.kr>
2024-03-11 11:26:40 +0900

    fix

--- src/main/java/com/cherryhouse/server/_core/security/JWTAuthenticationEntryPoint.java
+++ src/main/java/com/cherryhouse/server/_core/security/JWTAuthenticationEntryPoint.java
@@ -14,24 +14,6 @@ import java.io.IOException;
 @Component
 public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-//
-//    private final HandlerExceptionResolver resolver;
-//
-//    public JWTAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
-//        this.resolver = resolver;
-//    }
-//
-//    @Override
-//    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-//        log.warn("자격 증명이 필요합니다 - {}", authException.getMessage());
-//
-//        //log.info("여기2.. :{}",request.getAttribute("exception"));
-//        log.info("여기2.. :{}",request.getHeader("exception"));
-//        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-//        resolver.resolveException(request, response, null, (Exception) request.getAttribute("exception"));
-//        //response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-//    }
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         log.warn("자격 증명이 필요합니다 - {}", authException.getMessage());
