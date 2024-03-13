66c282e1d147ee277199cb0f50cd333338640b1c
xcelxlorx <xcelxlorx@gmail.com>
2024-03-13 09:03:37 +0900

    feat: Add schema to api response

--- src/main/java/com/cherryhouse/server/_core/util/ApiResponse.java
+++ src/main/java/com/cherryhouse/server/_core/util/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.cherryhouse.server._core.util;
 
 import com.cherryhouse.server._core.exception.ExceptionCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -27,8 +28,14 @@ public class ApiResponse {
     @Getter
     @AllArgsConstructor
     public static class ResponseDto<T>{
+
+        @Schema(description = "상태 코드", nullable = false, example = "200")
         private int status;
+
+        @Schema(description = "데이터", nullable = false)
         private T response;
+
+        @Schema(description = "에러 메시지", nullable = false, example = "null")
         private String errorMessage;
     }
 }
