4c7a8198a8118278185e42258ece497f7d8443a7
xcelxlorx <xcelxlorx@gmail.com>
2024-03-13 09:04:28 +0900

    feat: Add @Operation

--- src/main/java/com/cherryhouse/server/user/UserController.java
+++ src/main/java/com/cherryhouse/server/user/UserController.java
@@ -4,6 +4,7 @@ import com.cherryhouse.server._core.security.UserPrincipal;
 import com.cherryhouse.server._core.util.ApiResponse;
 import com.cherryhouse.server.user.dto.UserRequest;
 import com.cherryhouse.server.user.dto.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -24,12 +25,14 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "사용자 프로필 조회", description = "사용자의 프로필 정보를 가져옵니다.")
     @GetMapping("/{userId}")
     public ResponseEntity<?> getUserInfo(@PathVariable("userId") Long userId, @PageableDefault Pageable pageable){
         UserResponse.UserDto response = userService.getUserInfo(userId, pageable);
         return ResponseEntity.ok().body(ApiResponse.success(response));
     }
 
+    @Operation(summary = "사용자 프로필 수정", description = "사용자의 프로필 정보를 수정합니다.")
     @PutMapping("/info")
     public ResponseEntity<?> updateInfo(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                         @Valid @RequestBody UserRequest.UpdateInfoDto updateInfoDto, Errors errors){
@@ -37,6 +40,7 @@ public class UserController {
         return ResponseEntity.ok().body(ApiResponse.success());
     }
 
+    @Operation(summary = "사용자 프로필 이미지 수정", description = "사용자의 프로필 이미지를 수정합니다.")
     @PutMapping("/profileImg")
     public ResponseEntity<?> updateImg(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                         @RequestParam("file") MultipartFile file){
@@ -44,6 +48,7 @@ public class UserController {
         return ResponseEntity.ok().body(ApiResponse.success());
     }
 
+    @Operation(summary = "사용자 비밀번호 재설정", description = "사용자의 비밀번호를 재설정합니다.")
     @PutMapping("/password")
     public ResponseEntity<?> updatePwd(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                        @Valid @RequestBody UserRequest.UpdatePwdDto updatePwdDto, Errors errors){
@@ -53,6 +58,7 @@ public class UserController {
 
     //------------------------------------------------------
 
+    @Operation(summary = "사용자 스타일 업로드", description = "사용자의 스타일을 추가합니다.")
     @PostMapping("/style")
     public ResponseEntity<?> uploadStyle(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                          @RequestParam("file") List<MultipartFile> file){
@@ -60,6 +66,7 @@ public class UserController {
         return ResponseEntity.ok().body(ApiResponse.success());
     }
 
+    @Operation(summary = "사용자 스타일 삭제", description = "사용자의 스타일을 삭제합니다.")
     @DeleteMapping("/style")
     public ResponseEntity<?> deleteStyle(@AuthenticationPrincipal UserPrincipal userPrincipal,
                                           @RequestParam("file") String filePath){
